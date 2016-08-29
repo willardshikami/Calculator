3c965fda9098185436df91425f888ccfba9a9c50
willardshikami <willardshikami@gmail.com>
2016-08-29 10:49:42 +0300

    initial commit

--- app/src/main/java/com/example/willardpc/calculator/MainActivity.java
+++ app/src/main/java/com/example/willardpc/calculator/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.willardpc.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView _screen;
+    private String display = " ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        _screen = (TextView)findViewById(R.id.textView);
+        _screen.setText(display);
+
+    }
+
+    private void updateScreen() {
+        _screen.setText(display);
+    }
+
+    protected void onClickNumber(View v) {
+        Button b = (Button) v;
+        display += b.getText();
     }
 }
